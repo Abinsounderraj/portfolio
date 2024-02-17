80d6ded2d128a2b0b14bf97702deb6c187481562
Abin Sounderraj <abinsounderrajdev@gmail.com>
2024-02-17 10:31:35 +0530

    initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,189 @@
+export const Bio = {
+  name: "Abin Sounderraj",
+  roles: ["Developer", "Student", "Passionate Tech Explorer"],
+  description:
+    "As a student exploring technology, I'm eager to learn and contribute. Committed to delivering quality results, I'm ready to embrace opportunities and succeed in any role.",
+  github: "https://github.com/abinsounderraj/",
+  resume: "https://drive.google.com/",
+  linkedin: "https://www.linkedin.com/in/abinsounderraj/",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com/abinx.___/",
+  facebook: "https://www.facebook.com/",
+};
+
+
+
+export const skills = [
+  {
+    title: "Technical",
+    skills: [
+      
+    
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+   
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Google Cloud",
+        image:
+          "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      
+      
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      
+      
+    
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      // {
+      //   name: "Tenserflow",
+      //   image:
+      //     "https://static-00.iconduck.com/assets.00/tensorflow-icon-1911x2048-1m2s54vn.png",
+      // },
+
+      
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "Google Colab",
+        image: "https://colab.research.google.com/img/colab_favicon_256px.png"
+      },
+      
+      
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    role: "Role Title",
+    company: "Company Name",
+    date: "Start Date - End Date",
+    skills: ["Skill 1", "Skill 2", "Skill 3"],
+    image: "Image URL",
+    document: "Document URL",
+  },
+  {
+    id: 1,
+    role: "Role Title",
+    company: "Company Name",
+    date: "Start Date - End Date",
+    skills: ["Skill 1", "Skill 2", "Skill 3"],
+    image: "Image URL",
+    document: "Document URL",
+  },
+  // Add more experiences as needed
+];
+
+
+
+export const education = [
+  {
+    id: 0,
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/4/49/Anna_University_Logo.svg/1200px-Anna_University_Logo.svg.png",
+    school: "Anna University , Good Shepherd College of Engineering & Technology , Maruthamparai",
+    date: "2020 - 2024",
+    desc: " I am currently pursuing a Bachelor of Engineering in Computer Science and Engineering at Good Shepherd College of Engineering & Technology, Maruthamparai .",
+    degree: "Bachelor of Engineering",
+  },
+  {
+    id: 1,
+    image: "Image URL",
+    school: "MarBasil HSS & VHSS Senapathy",
+    date: "2018 - 2020",
+    desc: "During my Higher Secondary education at MarBasil HSS & VHSS Senapathy , I actively participated in Scout and Guide activities, honing my skills in teamwork and diligence.",
+    degree: "Higher Secondary (Plus Two)",
+  },
+  {
+    id: 2,
+    img: "",
+    school: "Montfort School CBSE , Chinnakanal",
+    date: "2006 -  2018",
+    desc: " Explored the world of computers and developed a passion for technology during my high school years.",
+    degree: "CBSE 10th Grade",
+  },
+];
+
+
+
+
+
